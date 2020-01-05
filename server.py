b6e031b6359f38e25485a1a348bce729cb36859d
David Vloedman <djvloedman@gmail.com>
2020-01-05 09:43:10 -0800

    Fleshed out some stubs

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
 from http.server import BaseHTTPRequestHandler
 class Server(BaseHTTPRequestHandler):
   def do_HEAD(self):
-    return
+    return 
+
   def do_POST(self):
-    return
+    self.respond()
+
   def do_GET(self):
     return
-  def handle_http(self):
-    return
+
+  def handle_http(self, status, content_type):
+    self.send_response(status)
+    self.send_header('Content-type', content_type)
+    self.end_headers()
+    return byte("Hello World", "UTF-8")
+
   def respond(self):
-    return
+    content = self.handle_http(200, 'text/html')
+    self.wfile.write(content)
+    
